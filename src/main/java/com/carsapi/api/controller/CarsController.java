41b981e3bec5f76b5564bba80fc7e36d44006ae4
Alice Amorim <alice.amorim@engenharia.ufjf.br>
2023-01-18 18:02:16 -0300

    create Put

--- src/main/java/com/carsapi/api/controller/CarsController.java
+++ src/main/java/com/carsapi/api/controller/CarsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class CarsController {
 
     @Autowired
     private CarRepository repository;
+    //instancia o repository
 
     @GetMapping
     public List<Car> listAll() {
@@ -29,6 +31,19 @@ public class CarsController {
     @PostMapping
     public void create(@RequestBody CarDTO req) {
         repository.save(new Car(req));
+    }
+
+    @PutMapping("/{id}")
+    public void putCar(@PathVariable Long id, @RequestBody CarDTO req) {
+        repository.findById(id).map(car -> {
+            car.setModelo(req.modelo());
+            car.setFabricante(req.fabricante());
+            car.setAnoModelo(req.anoModelo());
+            car.setDataFabricacao(req.dataFabricacao());
+            car.setValor(req.valor());
+
+            return repository.save(car);
+        });
     }
 
     @DeleteMapping("/{id}")
